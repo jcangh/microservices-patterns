376ca82a7ac362d3a267179ef0157301eea6687f
Juan Carlos Angulo <ing.jca@hotmail.com>
2023-02-23 16:49:26 -0500

    domain core implementation

--- food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Product.java
@@ -17,4 +17,8 @@ public class Product extends BaseEntity<ProductId> {
         this.price = price;
     }
 
+    public void updateWithConfirmedNameAndPrice(String name, Money price) {
+        this.name = name;
+        this.price = price;
+    }
 }
---
+++ food-ordering-system/order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderEvent.java
@@ -0,0 +1,16 @@
+package com.food.ordering.system.order.service.domain.event;
+
+import com.food.ordering.system.domain.event.DomainEvent;
+import com.food.ordering.system.order.service.domain.entity.Order;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.ZonedDateTime;
+
+@AllArgsConstructor
+@Getter
+public abstract class OrderEvent implements DomainEvent<Order> {
+
+    private final Order order;
+    private final ZonedDateTime createdAt;
+}
