fc317921a435faa76f6c1584b98621406fd1a1c8
Juan Carlos Angulo <ing.jca@hotmail.com>
2023-04-07 18:54:31 -0500

    Fix query order due to lazy initialization

--- food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/repository/OrderJpaRepository.java
+++ food-ordering-system/order-service/order-dataaccess/src/main/java/com/food/ordering/system/order/service/dataaccess/order/repository/OrderJpaRepository.java
@@ -1,7 +1,9 @@
 package com.food.ordering.system.order.service.dataaccess.order.repository;
 
 import com.food.ordering.system.order.service.dataaccess.order.entity.OrderEntity;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,5 +12,6 @@ import java.util.UUID;
 @Repository
 public interface OrderJpaRepository extends JpaRepository<OrderEntity, UUID> {
 
+    @Query("select o from OrderEntity o join fetch o.items where o.trackingId = :trackingId ")
     Optional<OrderEntity> findByTrackingId(UUID trackingId);
 }
